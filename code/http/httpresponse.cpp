88cf65806e08325220f946c3941149498f283ac8
AllwenWeill <1041268906@qq.com>
2023-01-10 09:07:02 -0800

    fix logo

--- code/http/httpresponse.cpp
+++ code/http/httpresponse.cpp
@@ -171,7 +171,7 @@ void HttpResponse::ErrorContent(Buffer& buff, string message)
     }
     body += to_string(code_) + " : " + status  + "\n";
     body += "<p>" + message + "</p>";
-    body += "<hr><em>TinyWebServer</em></body></html>";
+    body += "<hr><em>sv-JTracing</em></body></html>";
 
     buff.Append("Content-length: " + to_string(body.size()) + "\r\n\r\n");
     buff.Append(body);
